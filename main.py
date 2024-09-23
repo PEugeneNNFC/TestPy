4124f291658578f49670b3ac4b0e324d4bb2ca99
PEugeneNNFC <45099334+PEugeneNNFC@users.noreply.github.com>
2024-09-23 20:57:57 +0300

    T-6 Сделали вторую задачу. (#8)

--- main.py
+++ main.py
@@ -13,4 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Hello my freind!')
 
+    print_hi('Input firstname: ')
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
